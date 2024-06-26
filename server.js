e9c7499bdaec5e646c5a4baa858301ab6ae30587
FuxTool <fuxtoolz@proton.me>
2024-06-26 12:52:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,36 +13,65 @@ let users = {};
 
 io.on('connection', (socket) => {
     console.log('A user connected: ' + socket.id);
-    users[socket.id] = socket;
+    users[socket.id] = { socket: socket, busy: false };
 
     socket.on('offer', (data) => {
-        io.to(data.target).emit('offer', {
-            sdp: data.sdp,
-            source: socket.id
-        });
+        if (users[data.target]) {
+            users[data.target].socket.emit('offer', {
+                sdp: data.sdp,
+                source: socket.id
+            });
+            users[socket.id].busy = true;
+            users[data.target].busy = true;
+        }
     });
 
     socket.on('answer', (data) => {
-        io.to(data.target).emit('answer', {
-            sdp: data.sdp,
-            source: socket.id
-        });
+        if (users[data.target]) {
+            users[data.target].socket.emit('answer', {
+                sdp: data.sdp,
+                source: socket.id
+            });
+        }
     });
 
     socket.on('candidate', (data) => {
-        io.to(data.target).emit('candidate', {
-            candidate: data.candidate,
-            source: socket.id
-        });
+        if (users[data.target]) {
+            users[data.target].socket.emit('candidate', {
+                candidate: data.candidate,
+                source: socket.id
+            });
+        }
     });
 
     socket.on('disconnect', () => {
         console.log('User disconnected: ' + socket.id);
-        delete users[socket.id];
+        if (users[socket.id]) {
+            if (users[socket.id].busy) {
+                // Notify the other user that this user has disconnected
+                for (let id in users) {
+                    if (users[id].busy && id !== socket.id) {
+                        users[id].socket.emit('peer-disconnected');
+                        users[id].busy = false;
+                    }
+                }
+            }
+            delete users[socket.id];
+        }
     });
 
     socket.on('skip', () => {
-        let availableUsers = Object.keys(users).filter(id => id !== socket.id);
+        if (users[socket.id]) {
+            users[socket.id].busy = false;
+            // Notify the current peer that the call is ended
+            for (let id in users) {
+                if (users[id].busy && id !== socket.id) {
+                    users[id].socket.emit('peer-disconnected');
+                    users[id].busy = false;
+                }
+            }
+        }
+        let availableUsers = Object.keys(users).filter(id => id !== socket.id && !users[id].busy);
         if (availableUsers.length > 0) {
             let target = availableUsers[Math.floor(Math.random() * availableUsers.length)];
             socket.emit('new-peer', target);
